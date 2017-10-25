0613f37b2d19107e4a3fdaea9e4ec77268968b3c
JAreina <jareinafdez@gmail.com>
2017-10-25 20:35:56 +0200

    inicio ramatres

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ const gulp = require('gulp'),
                        pug:{
                        	     pretty:true,
                        	     locals :{
-                       	     	titulo: "Anier",
+                       	     	titulo: "Anierr",
                        	     	files:files
                        	     }
                        }
