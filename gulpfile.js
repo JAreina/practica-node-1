fc3a0a6337b74b638ef6b39b8336eb548433f251
JAreina <jareinafdez@gmail.com>
2017-10-28 20:13:39 +0200

    fin prueba

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ const gulp = require('gulp'),
 	pug = require('gulp-pug'),
 	sass = require('gulp-sass'),
 	babel = require('gulp-babel'),
+	//OPTIMIZAR IMAGENES
 	imagemin = require('gulp-imagemin'),
 	pngquant = require('imagemin-pngquant'),
 	svgmin = require('gulp-svgmin'),
 	webp = require('gulp-webp'),
+	//
 	useref = require('gulp-useref'),
 	concat = require('gulp-concat'),
 	uncss = require('gulp-uncss'),
@@ -24,7 +26,7 @@ const gulp = require('gulp'),
 
 	files ={
                    CSS : [
-                  ` ${dir.nm}/animate.css/animate.min.css`, // ruta a animate en node modules
+                  `${dir.nm}/animate.css/animate.min.css`, // ruta a animate en node modules
                  `${dir.nm}/font-awesome/css/font-awesome.min.css`,
 	     `${dir.nm}/responsimple/responsimple.min.css`,
 	      `${dir.nm}/owl.carousel/dist/assets/owl.carousel.min.css`,
@@ -56,7 +58,24 @@ const gulp = require('gulp'),
                        	     }
                        },
                        sass: {outputStyle: "compressed"},
-                       es6:{presets:['es2015']}
+                       es6:{presets:['es2015']},
+                       imagemin : {
+			progressive : true,
+			use : [ pngquant() ]
+		},
+		svgmin : {
+			plugins : [
+				{ convertColors : false },
+				{ removeAttrs : { attrs : ['fill'] } }
+			]
+		},
+
+		uncss : { html : [`${dir.dist}/*.html`] },
+		autoprefixer : {
+			browsers : ['last 5 versions'],
+			cascade : false
+		},
+		htmlmin : {collapseWhitespace: true}
 	};
 
 // tareas  EJECUTAR TAREAS en terminal : $ gulp pug
@@ -81,4 +100,65 @@ gulp.task('es6', () =>{
 	    .src( `${dir.src}/es6/*.js` )
 	      .pipe(babel(opts.es6))
 	      .pipe(gulp.dest(`${dir.dist}/js`))
+});
+
+
+//OPTIMIZAR IMAGESNES
+
+gulp.task('img', () => {
+	gulp
+		.src( `${dir.src}/img/**/*.+(png|jpeg|jpg|gif)` )
+		.pipe( imagemin(opts.imagemin) )
+		.pipe( gulp.dest(`${dir.dist}/img`) );
+});
+
+gulp.task('svg', () => {
+	gulp
+		.src( `${dir.src}/img/svg/*.svg` )
+		.pipe( svgmin(opts.svgmin) )
+		.pipe( gulp.dest(`${dir.dist}/img/svg`) );
+});
+
+gulp.task('webp', () => {
+	gulp
+		.src( `${dir.src}/img/**/*.+(png|jpeg|jpg)` )
+		.pipe( webp() )
+		.pipe( gulp.dest(`${dir.dist}/img/webp`) );
+});
+gulp.task('fonts', () => {
+	gulp
+		.src(files.fonts)
+		.pipe( gulp.dest(`${dir.dist}/fonts`) );
+});
+
+gulp.task('statics', () => {
+	gulp
+		.src(files.statics)
+		.pipe( gulp.dest(dir.dist) );
+});
+
+gulp.task('css', () => {
+	gulp
+		.src(files.CSS)
+		.pipe( concat(files.mCSS) )
+		.pipe( uncss(opts.uncss) )
+		.pipe( autoprefixer(opts.autoprefixer) )
+		.pipe( cleanCSS() )
+		.pipe( gulp.dest(`${dir.dist}/css`) );
+});
+
+gulp.task('js', () => {
+	gulp
+		.src( files.JS )
+		.pipe( concat(files.mJS) )
+		.pipe( uglify() )
+		.pipe( gulp.dest(`${dir.dist}/js`) );
+});
+
+gulp.task('html', () => {
+	gulp
+		.src(`${dir.dist}/*.html`)
+		.pipe( useref() )
+		.pipe( htmlmin(opts.htmlmin) )
+		.pipe( gulp.dest(dir.dist) );
 });
